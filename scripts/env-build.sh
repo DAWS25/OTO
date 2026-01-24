233820633204b907a83a1c8a9837faa65e754daa
Julio Faerman <julio@faermanj.com>
2026-01-24 09:08:36 +0000

    Refactor deployment scripts: replace mvnw command with env-build.sh sourcing

---
+++ scripts/env-build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+pushd "$DIR/.."
+echo "script [$0] started"
+#!
+
+./mvnw clean verify
+
+#!
+popd
+echo "script [$0] completed"
--- scripts/env-deploy.sh
+++ scripts/env-deploy.sh
@@ -4,7 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pushd "$DIR/.."
 #!
 
-./mvnw clean verify
+source ./env-build.sh
 
 aws sts get-caller-identity
 
