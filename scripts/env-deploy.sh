ce76f16fe5bc05886c8d1cc156f68758e97aa659
Julio Faerman <julio@faermanj.com>
2026-01-24 09:53:37 +0000

    Remove unnecessary 'sam init' command from env-deploy.sh

--- scripts/env-deploy.sh
+++ scripts/env-deploy.sh
@@ -9,7 +9,6 @@ source ./env-build.sh
 aws sts get-caller-identity
 
 pushd oto-sam
-sam init
 STACK_NAME="$ENV_ID-stack"
 sam depoly --resolve-s3 --stack-name "$STACK_NAME" --capabilities CAPABILITY_NAMED_IAM 
 popd
